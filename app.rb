b7130f9e1437b65c4d8035526f956ed52523d53a
Technicallyaustin <Technicallyaustin@gmail.com>
2020-06-28 15:30:29 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,66 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/' do 
+    "This is working"
+  end
+
+  get '/reversename/:name' do 
+    @user_name = params[:name]
+    @user_name.reverse
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    @squared = @number * @number 
+    "#{@squared}"
+
+  end
+
+  get '/say/:number/:phrase' do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    new_str = ""
+    @number.times do 
+      new_str += "#{@phrase} "
+    end
+    new_str 
+
+  end
+
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    
+    sentence = " #{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+    #sentence = @word1 + @word2 + @word3 + @word4 + @word5 + "."
+    sentence
+  end
+
+
+  get '/:operation/:number1/:number2' do
+  operations = params[:operations]
+  @number1 = params[:number1].to_i
+  @number2 = params[:number2].to_i
+
+  case operations
+  when 'add'
+    new = (@number1 + @number2).to_s
+    new
+  when 'subtract'
+    (@number1 - @number2).to_s
+  when 'multiply'
+    (@number1 * @number2).to_s
+  when 'divide'
+    (@number1 / @number2).to_s
+  end
+
+
+end
+
 
 end
